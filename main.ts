ed054f8bb53a04b4899220d4dac2cb8dbb0315fb
mauddabord <103410011+mauddabord@users.noreply.github.com>
2022-06-08 09:15:32 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,5 +5,14 @@ input.onButtonPressed(Button.B, function () {
     pins.digitalWritePin(DigitalPin.P14, 0)
 })
 basic.forever(function () {
-	
+    if (pins.digitalReadPin(DigitalPin.P13) == 1) {
+        pins.digitalWritePin(DigitalPin.P14, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P14, 0)
+    }
+    if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        pins.digitalWritePin(DigitalPin.P14, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P14, 0)
+    }
 })
